e88ed7cae60aaae162e5fe89fc3235da8c5d1bbc
Debabrata Das <debabratadas711@gmail.com>
2026-03-06 23:20:08 +0530

    06-03-2026

---
+++ main.ts
@@ -0,0 +1,13 @@
+// Boolean
+let isActive: boolean = true;
+let hasPermission = false;
+
+// Number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+let float: number = 3.14;
+
+let names: string = "Debabrata Das";
+console.log(names);

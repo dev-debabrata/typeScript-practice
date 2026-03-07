a44ed102e32d59b449c140351bceff8782b89dee
Debabrata Das <debabratadas711@gmail.com>
2026-03-07 22:00:59 +0530

    07-03-2026

---
+++ 01-basic.ts
@@ -0,0 +1,20 @@
+let names: string = "Debabrata Das";
+console.log(names);
+
+// Boolean
+let isActive: boolean = true;
+let hasPermission = false;
+
+// Number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+let float: number = 3.14;
+
+// String
+let color: string = "Blue";
+let fullName: string = "Debabrata Das";
+let age: number = 25;
+let sentence: string = `Hello, My name is ${fullName} and i'll be ${age + 1} next year.`;
+console.log(sentence);

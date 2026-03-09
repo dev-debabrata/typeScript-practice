babb9711972a472b76e90d9a980adb2f6061e4a4
Debabrata Das <debabratadas711@gmail.com>
2026-03-09 08:07:31 +0530

    add function

--- 04-functions.js
+++ 04-functions.js
@@ -6,3 +6,18 @@ function disp_details(id, name, mail_id) {
 }
 disp_details(111, "Debabrata Das");
 disp_details(112, "Soma Das", "Somadas@gmail.com");
+// Rest Parameter in Function
+function addNumbers() {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    var i;
+    var sum = 0;
+    for (i = 0; i < nums.length; i++) {
+        sum = sum + nums[i]; // sum += nums[i];
+    }
+    console.log("Sum of the numbers: ", sum);
+}
+addNumbers(1, 2, 3);
+addNumbers(10, 20, 30, 40);
--- 04-functions.ts
+++ 04-functions.ts
@@ -7,3 +7,17 @@ function disp_details(id: number, name: string, mail_id?: string) {
 
 disp_details(111, "Debabrata Das");
 disp_details(112, "Soma Das", "Somadas@gmail.com");
+
+// Rest Parameter in Function
+function addNumbers(...nums: number[]) {
+  let i;
+  let sum: number = 0;
+
+  for (i = 0; i < nums.length; i++) {
+    sum = sum + nums[i]; // sum += nums[i];
+  }
+  console.log("Sum of the numbers: ", sum);
+}
+
+addNumbers(1, 2, 3);
+addNumbers(10, 20, 30, 40);

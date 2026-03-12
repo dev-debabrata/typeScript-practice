d831507c745cc468a3f570e91fd10c20c2d09a80
Debabrata Das <debabratadas711@gmail.com>
2026-03-13 00:30:30 +0530

    13-03-2026

--- 07-classes.js
+++ 07-classes.js
@@ -4,9 +4,22 @@ var Person = /** @class */ (function () {
         this.age = age;
     }
     Person.prototype.greet = function () {
-        console.log("Hello " + this.nmae);
+        console.log("Hello! " + this.nmae);
     };
     return Person;
 }());
 var person = new Person("Debabrata Das", 25);
 person.greet();
+// Class
+var Person1 = /** @class */ (function () {
+    function Person1(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person1.prototype.introduce = function () {
+        return "Hi, my name is ".concat(this.name, " and I am ").concat(this.age, " years old.");
+    };
+    return Person1;
+}());
+var person1 = new Person1("Debabrata Das", 25);
+console.log(person1.introduce());
--- 07-classes.ts
+++ 07-classes.ts
@@ -14,3 +14,21 @@ class Person {
 
 const person = new Person("Debabrata Das", 25);
 person.greet();
+
+// Class
+class Person1 {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce(): string {
+    return `Hi, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+const person1 = new Person1("Debabrata Das", 25);
+console.log(person1.introduce());

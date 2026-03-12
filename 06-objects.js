d9826154e885be1a0e025c19aadd2b0468dd2d1a
Debabrata Das <debabratadas711@gmail.com>
2026-03-12 23:49:53 +0530

    add new

--- 06-objects.js
+++ 06-objects.js
@@ -13,3 +13,28 @@ console.log("Employee Object");
 console.log(employee.firstName);
 console.log(employee.lastName);
 console.log(employee.sayHi());
+function greet(person) {
+    return "Hello, ".concat(person.name, "!");
+}
+var user = {
+    name: "Debu",
+    age: 25,
+};
+console.log(greet(user));
+var myCar = {
+    make: "BMW",
+    model: "M5",
+    year: 2024,
+    electric: false,
+};
+console.log("I drive a ".concat(myCar.year, " ").concat(myCar.make, " ").concat(myCar.model, "."));
+var myBook = {
+    title: "TypeScript Basics",
+    author: {
+        name: "Debabrata Das",
+        birthYear: 1997,
+    },
+    pages: 330,
+    generation: "5th",
+};
+console.log("".concat(myBook.title, " written by ").concat(myBook.author.name, " and birthday year is ").concat(myBook.author.birthYear, "."));
--- 06-objects.ts
+++ 06-objects.ts
@@ -21,3 +21,65 @@ console.log("Employee Object");
 console.log(employee.firstName);
 console.log(employee.lastName);
 console.log(employee.sayHi());
+
+// Object Types
+interface Person1 {
+  name: string;
+  age: number;
+  address?: string;
+}
+
+function greet(person: Person1): string {
+  return `Hello, ${person.name}!`;
+}
+
+const user: Person1 = {
+  name: "Debu",
+  age: 25,
+};
+
+console.log(greet(user));
+
+// Defining a Car Object
+interface Car {
+  make: string;
+  model: string;
+  year: number;
+  electric?: boolean;
+}
+
+const myCar: Car = {
+  make: "BMW",
+  model: "M5",
+  year: 2024,
+  electric: false,
+};
+
+console.log(`I drive a ${myCar.year} ${myCar.make} ${myCar.model}.`);
+
+// Nested Object Types for a Book
+interface Author {
+  name: string;
+  birthYear: number;
+}
+
+interface Book {
+  title: string;
+  author: Author;
+  pages: number;
+  generation: string;
+}
+
+const myBook: Book = {
+  title: "TypeScript Basics",
+  author: {
+    name: "Debabrata Das",
+    birthYear: 1997,
+  },
+  pages: 330,
+  generation: "5th",
+};
+
+console.log(
+  `${myBook.title} written by ${myBook.author.name} and birthday year is ${myBook.author.birthYear}.`,
+);

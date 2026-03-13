15a2fd3765bfad23c3b50053c07c06bf92669b80
Debabrata Das <debabratadas711@gmail.com>
2026-03-13 22:51:47 +0530

    13-03-2026

--- 07-classes.js
+++ 07-classes.js
@@ -23,3 +23,34 @@ var Person1 = /** @class */ (function () {
 }());
 var person1 = new Person1("Debabrata Das", 25);
 console.log(person1.introduce());
+// Class with simple Object
+var Person2 = /** @class */ (function () {
+    function Person2(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person2.prototype.greet = function () {
+        console.log("Hello, I'm ".concat(this.name, " and I'm ").concat(this.age, " years old."));
+    };
+    return Person2;
+}());
+var person2 = new Person2("Debabrata Das", 25);
+console.log(person2.name);
+person2.greet();
+// Managing a Bank Account
+var BankAccount = /** @class */ (function () {
+    function BankAccount(accountHolder, balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+    BankAccount.prototype.deposit = function (amount) {
+        this.balance += amount;
+    };
+    BankAccount.prototype.getBalance = function () {
+        return "The balance for ".concat(this.accountHolder, " is ").concat(this.balance);
+    };
+    return BankAccount;
+}());
+var account = new BankAccount("Debabrata Das", 10000);
+account.deposit(5000);
+console.log(account.getBalance());
--- 07-classes.ts
+++ 07-classes.ts
@@ -32,3 +32,45 @@ class Person1 {
 
 const person1 = new Person1("Debabrata Das", 25);
 console.log(person1.introduce());
+
+// Class with simple Object
+class Person2 {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, I'm ${this.name} and I'm ${this.age} years old.`);
+  }
+}
+
+const person2 = new Person2("Debabrata Das", 25);
+console.log(person2.name);
+person2.greet();
+
+// Managing a Bank Account
+class BankAccount {
+  accountHolder: string;
+  balance: number;
+
+  constructor(accountHolder: string, balance: number) {
+    this.accountHolder = accountHolder;
+    this.balance = balance;
+  }
+
+  deposit(amount: number): void {
+    this.balance += amount;
+  }
+
+  getBalance(): string {
+    return `The balance for ${this.accountHolder} is ${this.balance}`;
+  }
+}
+
+const account = new BankAccount("Debabrata Das", 10000);
+account.deposit(5000);
+console.log(account.getBalance());

c12e62f867012c75338d6aa0d298f955798a32aa
Debabrata Das <debabratadas711@gmail.com>
2026-03-14 23:31:04 +0530

    added

--- 09-readOnly-properties.js
+++ 09-readOnly-properties.js
@@ -12,3 +12,19 @@ var obj = new ReadonlyExample("Debu");
 console.log(obj.getName());
 var p1 = { x: 10, y: 20 };
 console.log("Point Coordinate: ".concat(p1.x, ", ").concat(p1.y));
+// Readonly Array of numbers
+var numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+// Readonly Property in a class
+var Car = /** @class */ (function () {
+    function Car(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+    Car.prototype.getCarInfo = function () {
+        return "This is a ".concat(this.make, " car and this model is ").concat(this.model);
+    };
+    return Car;
+}());
+var myCar = new Car("BMW", "M5");
+console.log(myCar.getCarInfo());
--- 09-readOnly-properties.ts
+++ 09-readOnly-properties.ts
@@ -22,3 +22,25 @@ interface Point {
 
 const p1: Point = { x: 10, y: 20 };
 console.log(`Point Coordinate: ${p1.x}, ${p1.y}`);
+
+// Readonly Array of numbers
+const numbers: ReadonlyArray<number> = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// Readonly Property in a class
+class Car {
+  readonly make: string;
+  readonly model: string;
+
+  constructor(make: string, model: string) {
+    this.make = make;
+    this.model = model;
+  }
+
+  getCarInfo(): string {
+    return `This is a ${this.make} car and this model is ${this.model}`;
+  }
+}
+
+const myCar = new Car("BMW", "M5");
+console.log(myCar.getCarInfo());

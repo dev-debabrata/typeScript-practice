a715ae76cb574d8a59bfcdcfd3f425c1c80d93e9
Debabrata Das <debabratadas711@gmail.com>
2026-03-15 00:41:58 +0530

    15-03-2026

--- 10-abstract-classes-and-methods.js
+++ 10-abstract-classes-and-methods.js
@@ -35,3 +35,47 @@ var Dog = /** @class */ (function (_super) {
 var myDog = new Dog();
 myDog.makeSound();
 myDog.move();
+// Abstract Class with Abstract Property
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.display = function () {
+        console.log(this.name);
+    };
+    return Person;
+}());
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, empCode) {
+        var _this = _super.call(this) || this;
+        _this.name = name;
+        _this.empCode = empCode;
+        return _this;
+    }
+    return Employee;
+}(Person));
+var emp = new Employee("Debabrata Das", 101);
+emp.display();
+// Abstract Class with Abstract Method
+var Shape = /** @class */ (function () {
+    function Shape() {
+    }
+    Shape.prototype.printArea = function () {
+        console.log("The Area is: ".concat(this.getArea()));
+    };
+    return Shape;
+}());
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle(radius) {
+        var _this = _super.call(this) || this;
+        _this.radius = radius;
+        return _this;
+    }
+    Circle.prototype.getArea = function () {
+        return Math.PI * this.radius * this.radius;
+    };
+    return Circle;
+}(Shape));
+var circle = new Circle(5);
+circle.printArea();
--- 10-abstract-classes-and-methods.ts
+++ 10-abstract-classes-and-methods.ts
@@ -16,3 +16,50 @@ class Dog extends Animal {
 const myDog = new Dog();
 myDog.makeSound();
 myDog.move();
+
+// Abstract Class with Abstract Property
+abstract class Person {
+  abstract name: string;
+  display(): void {
+    console.log(this.name);
+  }
+}
+
+class Employee extends Person {
+  name: string;
+  empCode: number;
+
+  constructor(name: string, empCode: number) {
+    super();
+    this.name = name;
+    this.empCode = empCode;
+  }
+}
+
+const emp = new Employee("Debabrata Das", 101);
+emp.display();
+
+// Abstract Class with Abstract Method
+abstract class Shape {
+  abstract getArea(): number;
+
+  printArea(): void {
+    console.log(`The Area is: ${this.getArea()}`);
+  }
+}
+
+class Circle extends Shape {
+  radius: number;
+
+  constructor(radius: number) {
+    super();
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+const circle = new Circle(5);
+circle.printArea();
